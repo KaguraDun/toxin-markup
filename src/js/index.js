0e46961ac5cb7467b8c83c4f1837121076aae5ed
Vasily Kovnev <v.k95@yandex.ru>
2021-05-12 21:41:08 +0300

    feat: add dropdown with guests

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
+import Dropdown from '../components/Dropdown/Dropdown';
 
 function importAll(r) {
   r.keys().forEach(r);
@@ -6,3 +7,19 @@ function importAll(r) {
 
 importAll(require.context('../components/', true, /\.js|.scss$/));
 importAll(require.context('../pug-mixins/', true, /\.js|.scss$/));
+
+const dropdownGuests = document.querySelector('.dropdown-guests');
+
+if (dropdownGuests) {
+  const properties = {
+    name: 'Сколько гостей',
+    label: 'dropdown',
+    options: [
+      { name: 'Взрослые', value: 1 },
+      { name: 'Дети', value: 2 },
+      { name: 'Младенцы', value: 3 },
+    ],
+  };
+  const dropdown = new Dropdown(dropdownGuests, properties);
+  dropdown.init();
+}
