d5c2c8b92938a27c34b5495e8398c4430a63c851
Vasily Kovnev <v.k95@yandex.ru>
2021-05-17 20:57:06 +0300

    feat: add date dropdown mixin
    add litepicker plugin

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../styles/index.scss';
 import 'jquery';
 import 'jquery-mask-plugin';
 
+import Litepicker from 'litepicker';
 import Dropdown from '../components/Dropdown/Dropdown';
 
 function importAll(r) {
@@ -32,3 +33,46 @@ if (dropdownGuests) {
   const dropdown = new Dropdown(dropdownGuests, properties);
   dropdown.init();
 }
+
+new Litepicker({
+  element: document.querySelector('.js-date-dropdown-arrival'),
+  elementEnd: document.querySelector('.js-date-dropdown-departure'),
+  singleMode: false,
+  autoApply: false,
+  allowRepick: true,
+  autoRefresh: true,
+  minDate: Date.now(),
+  disallowLockDaysInRange: true,
+  buttonText: { apply: 'применить', cancel: 'очистить' },
+});
+
+new Litepicker({
+  element: document.querySelector('.js-date-dropdown'),
+  allowRepick: true,
+  autoApply: false,
+  singleMode: false,
+  autoRefresh: true,
+  minDate: Date.now(),
+  buttonText: { apply: 'применить', cancel: 'очистить' },
+});
+
+// const dropdownRoomAmenity = document.querySelector('.dropdown-room-amenity');
+
+// if (dropdownRoomAmenity) {
+//   const properties = {
+//     placeholder: 'Удобства',
+//     label: 'dropdown',
+//     options: {
+//       Спальни: { count: 2, pattern: ['Спальня', 'Спальни', 'Спален'] },
+//       Кровати: { count: 2, pattern: ['Кровать', 'Кровати', 'Кроватей'] },
+//       'Ванные комнаты': {
+//         count: 0,
+//         pattern: ['Ванная комната', 'Ванных комнаты', 'Ванных комнат'],
+//       },
+//     },
+//   };
+
+//   const dropdown = new Dropdown(dropdownRoomAmenity, properties);
+//   dropdown.init();
+//   dropdown.dropdownItems.hidden = false;
+// }
