7750a62b9ccb4085701095853493a24edfcf9cd8
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-11-19 18:52:07 +0300

    feat: add sign-up component

---
+++ src/components/Sign-up/Sign-up.js
@@ -0,0 +1,5 @@
+import './Sign-up.scss';
+import '@/components/Input/Input.js';
+import '@/components/Input-radio/Input-radio.js';
+import '@/components/Input-toggle/Input-toggle.js';
+import '@/components/Button/Button.js';
