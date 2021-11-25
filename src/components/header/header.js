bfeb5ac9bf5b594c2aa2399e2fb247d62a10cb31
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-11-25 13:39:33 +0300

    feat: add burger menu to header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,3 +2,18 @@ import './header.scss';
 import '@/components/logo/logo.js';
 import '@/components/header-nav/header-nav.js';
 import '@/components/button/button.js';
+
+(() => {
+  const burgerMenu = document.querySelector('.header__burger');
+
+  function handleBurgerMenuClick() {
+    const openModifier = 'header__burger--open';
+    const headerNavigation = document.querySelector('.header__right-col');
+    headerNavigation.classList.toggle(openModifier);
+    burgerMenu.classList.toggle(openModifier);
+  }
+
+  if (burgerMenu) {
+    burgerMenu.addEventListener('click', handleBurgerMenuClick);
+  }
+})();
