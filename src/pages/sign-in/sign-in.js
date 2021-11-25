6b4f6a3aff49cd52f21bb2a2f95157700b0a4dd7
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-11-25 16:03:29 +0300

    feat: add sign-in page

---
+++ src/pages/sign-in/sign-in.js
@@ -0,0 +1,6 @@
+import '@/components/header/header.js';
+import '@/components/sign-in/sign-in.js';
+import '@/components/footer/footer.js';
+
+import '@/styles/_common.scss';
+import './sign-in.scss';
