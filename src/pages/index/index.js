50eea6f59bb4c2e368017d7f423aa0b83068d351
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-11-26 15:18:23 +0300

    feat: add landing page

---
+++ src/pages/index/index.js
@@ -0,0 +1,6 @@
+import '@/components/header/header.js';
+import '@/components/find-rooms/find-rooms.js';
+import '@/components/footer/footer.js';
+
+import '@/styles/_common.scss';
+import './index.scss';
