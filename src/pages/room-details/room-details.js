f9534eefccd45bd53546b6b0adda42f55cbe37b0
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-03 18:17:15 +0300

    feat: add room details page

---
+++ src/pages/room-details/room-details.js
@@ -0,0 +1,18 @@
+import createChart from '@/components/room-impressions/room-impressions.js';
+
+import '@/components/header/header.js';
+import '@/components/room-advantages/room-advantages.js';
+import '@/components/book-room/book-room.js';
+import '@/components/review-list/review-list.js';
+import '@/components/bullet-list/bullet-list.js';
+import '@/components/footer/footer.js';
+
+import '@/styles/_common.scss';
+import './room-details.scss';
+
+(() => {
+  createChart({
+    selector: '.js-room-details-page__room-impressions',
+    values: [0, 65, 65, 130],
+  });
+})();
