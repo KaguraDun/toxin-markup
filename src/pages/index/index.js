b3e6a047d4d32918e3b44a77e37b7b8a52a26680
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-10 20:37:18 +0300

    feat: add site pages navigation to index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,3 @@
-import '@/components/header/header.js';
-import '@/components/find-rooms/find-rooms.js';
-import '@/components/footer/footer.js';
+import '@/components/logo/logo.js';
 import '@/styles/_common.scss';
 import './index.scss';
