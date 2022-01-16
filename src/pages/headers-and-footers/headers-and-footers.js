0200488cf3d1d842f78c2e48adbf70ffc693894d
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-16 17:05:31 +0300

    refactor: headers and footers page
    refactor: remove style change of child blocks by parent

--- src/pages/headers-and-footers/headers-and-footers.js
+++ src/pages/headers-and-footers/headers-and-footers.js
@@ -1,6 +1,8 @@
 import '@/components/logo/logo.js';
 import '@/components/header/header.js';
 import '@/components/footer/footer.js';
+import '@/components/copyright/copyright.js';
+import '@/components/socials/socials.js';
 import '@/assets/styles/_common.scss';
 
 import './headers-and-footers.scss';
