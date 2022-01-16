9f7bc03be84b8fc5412bff0d2c0fa056f6568a0c
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-16 19:29:28 +0300

    refactor: rename class names in dropdown component

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -26,17 +26,16 @@ class Dropdown {
     Object.keys(this.properties.options).forEach((key) => {
       const value = this.properties.options[key].count;
       const buttonMinusDisabled = value <= 0 ? 'disabled' : '';
-
+      const buttonClassNames = 'dropdown__button-minus js-dropdown__button-minus';
       items += `
       <li class="dropdown__item">
         <div class="dropdown__item-name">${key}</div>
         <div class="dropdown__item-buttons">
-          <button type="button" class="button-minus js-button-minus" ${buttonMinusDisabled}>
-          </button>
+          <button type="button" class="${buttonClassNames}" ${buttonMinusDisabled}></button>
           <div class="dropdown__item-value js-dropdown__item-value" data-value="${key}">
             ${value}
           </div>
-          <button type="button" class="button-plus js-button-plus"></button>
+          <button type="button" class="dropdown__button-plus js-dropdown__button-plus"></button>
         </div>
       </li>`;
     });
@@ -51,8 +50,12 @@ class Dropdown {
   static createControlButtons() {
     return `
     <div class="dropdown__button-wrapper">
-      <button type="button" class="button-widget js-button-clear">Очистить</button>
-      <button type="button" class="button-widget js-button-apply">Применить</button>
+      <button type="button" class="dropdown__button-widget js-dropdown__button-clear">
+        Очистить
+      </button>
+      <button type="button" class="dropdown__button-widget js-dropdown__button-apply">
+        Применить
+      </button>
     </div>`;
   }
 
@@ -66,7 +69,7 @@ class Dropdown {
       <p class="dropdown__label">${this.properties.label ?? ''}</p>
       <div class="dropdown__input-wrapper js-dropdown__input-wrapper">
         <input type="text" class="${dropdownInputClass}" placeholder="${placeholder}" readonly>
-        <span class="dropdown__button-icon"></span>
+        <span class="dropdown__button-icon_style_expand-more"></span>
       </div>
       <ul class="dropdown__items js-dropdown__items" ${isExpanded ? '' : 'hidden'}>
         ${this.createItems()}
@@ -78,7 +81,7 @@ class Dropdown {
   hideButtonClear(isHidden) {
     if (!this.buttonClear) return;
 
-    const modifier = 'button_hidden';
+    const modifier = 'dropdown__button_hidden';
     this.buttonClear.classList.toggle(modifier, isHidden);
   }
 
@@ -89,8 +92,8 @@ class Dropdown {
     this.dropdownItems = this.element.querySelector('.js-dropdown__items');
 
     const dropdownInputWrapper = this.element.querySelector('.js-dropdown__input-wrapper');
-    this.buttonClear = this.element.querySelector('.js-button-clear');
-    this.buttonApply = this.element.querySelector('.js-button-apply');
+    this.buttonClear = this.element.querySelector('.js-dropdown__button-clear');
+    this.buttonApply = this.element.querySelector('.js-dropdown__button-apply');
 
     dropdownInputWrapper.addEventListener('click', this.handleDropdownExpand);
     this.dropdownItems.addEventListener('click', this.handleStoreValueChange);
@@ -108,8 +111,8 @@ class Dropdown {
 
     const valueElement = parent.querySelector('.js-dropdown__item-value');
     const { value } = valueElement.dataset;
-    const buttonMinus = parent.querySelector('.js-button-minus');
-    const buttonPlus = parent.querySelector('.js-button-plus');
+    const buttonMinus = parent.querySelector('.js-dropdown__button-minus');
+    const buttonPlus = parent.querySelector('.js-dropdown__button-plus');
 
     if (e.target === buttonMinus) {
       this.store[value].count -= 1;
@@ -138,7 +141,7 @@ class Dropdown {
     });
 
     const valueElements = this.element.querySelectorAll('.js-dropdown__item-value');
-    const buttonsMinus = this.element.querySelectorAll('.js-button-minus');
+    const buttonsMinus = this.element.querySelectorAll('.js-dropdown__button-minus');
 
     valueElements.forEach((element) => {
       element.innerText = 0;
