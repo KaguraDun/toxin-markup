29ce5b7f4ccf5d1c28f0b0eeb36557380b40dbbd
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-18 18:00:44 +0300

    refactor: rewrite button like component as class

---
+++ src/components/button-like/ButtonLike.js
@@ -0,0 +1,33 @@
+class ButtonLike {
+  constructor(element) {
+    this.element = element;
+    this.likesNumberElement = null;
+    this.likesNumber = 0;
+  }
+
+  addEventListener() {
+    this.likesNumberElement = this.element.querySelector('.js-button-like__likes-number');
+    this.likesNumber = Number(this.likesNumberElement.innerText);
+
+    this.element.addEventListener('click', this.handleLikeButtonClick);
+  }
+
+  handleLikeButtonClick = (e) => {
+    const target = e.target.closest('.js-button-like');
+
+    if (target) {
+      const isButtonPressed = target.classList.toggle('button-like_pressed');
+      this.toggleLikesNumber(isButtonPressed);
+    }
+  };
+
+  toggleLikesNumber(isButtonPressed) {
+    this.likesNumber = isButtonPressed ? this.likesNumber + 1 : this.likesNumber - 1;
+
+    if (this.likesNumber < 0) this.likesNumber = 0;
+
+    this.likesNumberElement.innerText = this.likesNumber;
+  }
+}
+
+export default ButtonLike;
