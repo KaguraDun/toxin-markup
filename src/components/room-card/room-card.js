3e147465fe2e5140e9fe907f2772477ff85e94a0
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-23 17:45:56 +0300

    feat: add slider component to room card  #13
    feat: update props on cards page #13

--- src/components/room-card/room-card.js
+++ src/components/room-card/room-card.js
@@ -1,4 +1,16 @@
+import Slider from '@/components/slider/Slider.js';
 import '@/components/room-info/room-info.js';
 import '@/components/button-rate/button-rate.js';
 
 import './room-card.scss';
+
+(() => {
+  const sliders = document.querySelectorAll('.js-swiper');
+
+  if (sliders.length > 0) {
+    sliders.forEach((sliderContainer) => {
+      const slider = new Slider(sliderContainer);
+      slider.init();
+    });
+  }
+})();
