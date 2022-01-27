adb46c9e27e0200468f2ea5c0a7fb8c143e053ff
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-27 23:22:07 +0300

    refactor: colors and type page
    refactor: reduce repeated code
    refactor: replace px with rem
    refactor: rename variables

--- src/pages/colors-and-type/colors-and-type.js
+++ src/pages/colors-and-type/colors-and-type.js
@@ -1,5 +1,6 @@
 import '@/components/logo/logo.js';
 import '@/components/color-example/color-example.js';
+import '@/components/type-example/type-example.js';
 import '@/styles/_common.scss';
 
 import './colors-and-type.scss';
